9ce6b505d0993ed3fb49cc0a7ea2b4a0affc3b41
Amaya <98253047+AmayaB2011@users.noreply.github.com>
2026-05-11 10:37:03 +1000

    Update script.js

--- script.js
+++ script.js
@@ -262,6 +262,6 @@ document.getElementById('playAgain').addEventListener('click', () => {
     highestNumber = 0;
     document.getElementById("score").innerText = score;
     document.querySelectorAll('.tile').forEach(el => el.remove());
-    document.querySelectorAll('.block').forEach(el => el.style.display = 'none');
+    document.querySelectorAll('.block').forEach(el => el.style.opacity = '0');
     startGame();
 });
